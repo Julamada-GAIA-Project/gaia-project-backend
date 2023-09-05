9a3904199ba3112a2b36084c4c5f61b9330f66f8
R-dVL <rauldel.valle.lima@gmail.com>
2023-09-05 23:03:10 +0200

    Create endpoits for azure cosmodb

--- src/main.py
+++ src/main.py
@@ -1,27 +1,51 @@
 from fastapi import FastAPI
+from azure.cosmos import CosmosClient, exceptions
 import json
 import random
+    
+## COSMODB CONNECTION ##
+# CosmoDB Params
+endpoint = ""
+key = ""
+database_name = ""
+container_name = ""
 
-from models.Human import Human
+# Cosmo Client
+client = CosmosClient(endpoint, key)
 
-with open('resources/stats.json') as statsJson:
-    stats = json.load(statsJson)
+# Obtains BDD or creates it
+try:
+    database = client.get_database_client(database_name)
+except exceptions.CosmosResourceNotFoundError:
+    database = client.create_database_if_not_exists(database_name)
+    
+try:
+    container = database.get_container_client(container_name)
+except exceptions.CosmosResourceNotFoundError:
+    container = database.create_container_if_not_exists(id=container_name)
 
-name = random.choice(stats['human']['names'])
-lastName = random.choice(stats['human']['lastNames'])
-physical = random.choice(stats['human']['physical'])
-studies = random.choice(stats['human']['studies'])
-
-human = Human(name, lastName, physical, studies)
-
+# Init
 app = FastAPI()
 
+## ENDPOINTS ##
+# Post Human
+@app.post("/humans")
+async def post_human(data: dict):
+    response = container.create_item(body=data)
+    return {"message": "Human Post success", "item_id": response["id"]}
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+# Post Random Human
+@app.post("/humans/random")
+async def post_human():
+    with open('resources/stats.json') as statsJson:
+        stats = json.load(statsJson)
 
+    data = {
+        "name": random.choice(stats['human']['names']),
+        "lastName": random.choice(stats['human']['lastNames']),
+        "physical": random.choice(stats['human']['physical']),
+        "studies": random.choice(stats['human']['studies'])
+    }
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+    response = container.create_item(body=data)
+    return {"message": "Datos insertados en Cosmos DB", "item_id": response["id"]}
